e28dcac1958c88462104ba29ac422481a39f75b4
GitYTP <soujanyabandari88@gmail.com>
2021-01-31 16:41:32 +0530

    simple maven

---
+++ libraryManagementApp/src/main/java/com/ytp/pack/Book.java
@@ -0,0 +1,48 @@
+package com.ytp.pack;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="books")
+public class Book {
+	@Id
+private int book_id;
+private String book_name;
+private String book_author;
+public Book(int book_id, String book_name, String book_author) {
+	super();
+	this.book_id = book_id;
+	this.book_name = book_name;
+	this.book_author = book_author;
+}
+public Book()
+{
+	
+}
+public int getBook_id() {
+	return book_id;
+}
+public void setBook_id(int book_id) {
+	this.book_id = book_id;
+}
+public String getBook_name() {
+	return book_name;
+}
+public void setBook_name(String book_name) {
+	this.book_name = book_name;
+}
+public String getBook_author() {
+	return book_author;
+}
+public void setBook_author(String book_author) {
+	this.book_author = book_author;
+}
+
+
+
+
+
+		
+}
